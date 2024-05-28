3e9387248e1a9ab59828e3bbfc078529fa8b2974
tris <kaynguywn22898@gmail.com>
2024-05-28 20:16:35 +0700

    Fix product detail

--- src/page/productdetail.tsx
+++ src/page/productdetail.tsx
@@ -1,24 +1,78 @@
-import React from 'react'
-import Detail from '../component/Product/detail'
-import Product from '../component/product'
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { getProductsDetail } from "../services/adminService";
+import { useParams } from "react-router-dom";
+import CloudinaryDetailImg from "../component/Cloudinary/CloudImg";
 
-export default function Productdetail() {
+interface Product {
+    ProductId: number;
+    ProductName: string;
+    CatalogName: string;
+    ImageURL: string;
+    CatalogId: number;
+    Quantity: number;
+    Price: number;
+    Brand: string;
+    Model: string;
+    Processor: string;
+    RAMSize: number;
+    StorageSize: number;
+    GraphicsCard: string;
+    ReleaseDate: string;
+    Details: any;
+}
+
+const ProductDetail = () => {
+    const [productDetail, setProductDetail] = useState<Product | null>(null);
     const { id } = useParams();
 
-    const pdid = id
-    return (
-        <>  
-            {/* <h1>{id}</h1> */}
-            <div className="font-[sans-serif] bg-white">
-                <div className="p-6 lg:max-w-7xl max-w-4xl mx-auto">
-                    {/* img and name price */}
-                    <Product id={pdid}/>
+    useEffect(() => {
+        const fetchDetailProducts = async () => {
+            try {
+                if (typeof id === 'string') {
+                    const productsData = await getProductsDetail(parseInt(id, 10));
+                    setProductDetail(productsData);
+                }
+            } catch (err) {
+                console.error("Error fetching product details:", err);
+            }
+        };
 
-                    {/* detail */}
-                    <Detail id={pdid}/>
+        if (id) {
+            fetchDetailProducts();
+        }
+    }, [id]); // Add id as a dependency
+
+    return (
+        <div className="container mx-auto px-4">
+            {productDetail ? (
+                <div key={productDetail.ProductId} className="grid grid-cols-1 lg:grid-cols-2 gap-12 shadow-lg p-6 bg-white rounded-lg">
+                    <div className="flex justify-center items-center">
+                        <div className="max-w-md">
+                            <CloudinaryDetailImg ImgUrl={productDetail.ImageURL} />
+                        </div>
+                    </div>
+                    <div className="space-y-6">
+                        <h2 className="text-2xl font-bold text-gray-800">{productDetail.ProductName}</h2>
+                        <p className="text-gray-600">${productDetail.Price}</p>
+                        <div>
+                            <h3 className="text-lg font-bold text-gray-800 mt-6">Product information</h3>
+                            <ul className="mt-2 text-gray-600">
+                                <li><span className="font-semibold">Brand:</span> {productDetail.Details.Brand}</li>
+                                <li><span className="font-semibold">Model:</span> {productDetail.Details.Model}</li>
+                                <li><span className="font-semibold">Processor:</span> {productDetail.Details.Processor}</li>
+                                <li><span className="font-semibold">RAM:</span> {productDetail.Details.RAMSize} GB</li>
+                                <li><span className="font-semibold">Storage Size:</span> {productDetail.Details.StorageSize} GB</li>
+                                <li><span className="font-semibold">Graphics Card:</span> {productDetail.Details.GraphicsCard}</li>
+                            </ul>
+                        </div>
+                        <button type="button" className="w-full py-2.5 bg-gray-800 text-white font-semibold rounded hover:bg-gray-700">Add to cart</button>
+                    </div>
                 </div>
-            </div>
-        </>
-    )
+            ) : (
+                <p className="text-center mt-8">Loading...</p>
+            )}
+        </div>
+    );
 }
+
+export default ProductDetail;
